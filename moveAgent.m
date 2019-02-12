5ed9dbde2d9da095b924221866fced277fcff67a
EmiljanQ3 <emiljan97@hotmail.com>
2019-02-12 15:39:12 +0100

    Fixed recusion issue

--- moveAgent.m
+++ moveAgent.m
@@ -1,4 +1,10 @@
 function resultPos = moveAgent(pos,targetPos,obstacle,L, threshold)
+
+    %If movement is small enough, return current position.
+    if(norm(pos-targetPos) < threshold)
+        resultPos = pos;
+        return
+    end
     
     impactDist = inf;       %Distance to closest wall collision.
     impactPoint = NaN;      %Point of collision
@@ -34,13 +40,7 @@ function resultPos = moveAgent(pos,targetPos,obstacle,L, threshold)
     normal = tangent*[0,-1;1,0];
 
     %Calculate new target pos based on tangental movement along the wall.
-    targetPos = impactPoint + tangent*dot((pos-impactPoint), tangent) - 0.00001*normal*dot((pos-impactPoint), normal);
-
-    %If movement is small enough, return current position.
-    if(norm(impactPoint-targetPos) < threshold)
-        resultPos = pos;
-        return
-    end
+    targetPos = impactPoint + tangent*dot((pos-impactPoint), tangent) - 0.00001*normal*dot((pos-impactPoint), normal);  
 
     resultPos = moveAgent(impactPoint, targetPos, obstacle, L, threshold);
 end
