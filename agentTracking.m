31d28356e32a8e2082794edfa1f3d1e2570e427b
EbbaEkblom <ekebba@student.chalmers.se>
2019-02-20 10:27:04 +0100

    Sets size to largets time value

--- agentTracking.m
+++ agentTracking.m
@@ -1,10 +1,21 @@
 function M = agentTracking(file)
-
+    % Imports an xml-file
+    % Converts the file to a matrix:
+    % 1st dimenstion is the time length of the tracking
+    % 2nd dimension is the number of parameters for each agent (t,x,y,z)
+    % 3rd dimension is the number of agents (found by the tracking
+    % programme)
     allTracks = importTrackMateTracks(file);
     length(allTracks);
-    M = zeros(893,4,length(allTracks));
+    max_t = 1;
+    for i = 1:length(allTracks)
+        if (max(size(allTracks{i})) > max_t)
+            max_t = max(size(allTracks{i}));
+        end
+    
+    M = zeros(max_t,4,length(allTracks));
     lengths = zeros(length(allTracks));
-    hold on
+    hold on    
     for i = 1:length(allTracks)
         m = cell2mat(allTracks(i));
         lengths(i) = size(m,1);
