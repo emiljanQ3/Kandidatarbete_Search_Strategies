024183edd60a795465c6f2c2626d5ab69d01e8f3
EbbaEkblom <ekebba@student.chalmers.se>
2019-02-21 14:30:02 +0100

    Matrix in the same way as a_pos matrix

--- agentTracking.m
+++ agentTracking.m
@@ -1,10 +1,11 @@
-function M = agentTracking(file)
+function [M,lengths] = agentTracking(file)
     % Imports an xml-file
     % Converts the file to a matrix:
-    % 1st dimenstion is the time length of the tracking
-    % 2nd dimension is the number of parameters for each agent (t,x,y,z)
-    % 3rd dimension is the number of agents (found by the tracking
-    % programme)
+    % 1st dimenstion is the number of agents (found by the tracking
+    % programme)is the time length of the tracking
+    % 2nd dimension is the time length of the trackingis the number of parameters for each agent (t,x,y,z)
+    % 3rd dimension is the number of parameters for each agent (t,x,y,z)
+
     allTracks = importTrackMateTracks(file);
     length(allTracks);
     max_t = 1;
@@ -13,16 +14,17 @@ function M = agentTracking(file)
             max_t = max(size(allTracks{i}));
         end
     
-    M = zeros(max_t,4,length(allTracks));
-    lengths = zeros(length(allTracks));
+    M = zeros(length(allTracks),2,max_t);
+    lengths = zeros(length(allTracks),1);
     hold on    
     for i = 1:length(allTracks)
         m = cell2mat(allTracks(i));
         lengths(i) = size(m,1);
-        M(1:size(m,1),:,i) = m;
+        m_temp = m(:,2:3)';
+        M(i,:,1:size(m,1)) = m_temp;
         plot(m(:,2),m(:,3))
     end
     %lengths(18)
-    %plot(M(1:lengths(18),2,18),M(1:lengths(18),3,18))
+    %plot(M(18,1:lengths(18),2),M(18,1:lengths(18),3))
 end
 
