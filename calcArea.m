b9a10e8c804eee7b00e8afc1d45bc6fa8dd6a108
EbbaEkblom <ekebba@student.chalmers.se>
2019-03-03 13:39:51 +0100

    Can now handle diagonal movement

--- calcArea.m
+++ calcArea.m
@@ -5,17 +5,9 @@ function [numSquares,A] = calcArea(M,vdT)
 % The input Matrix should not contain any NaN, should be complete
 % trajectories.
 
-% dT = 0.04; % 1/25
-% v = 0;
-% for i = 1:size(M,1)
-%     [k, v_ny] = getCirality(M(i,:,2:11),t_step);
-%     if v_ny > v
-%         v = v_ny
-%     end
-% end
-%l = 1.5 * dT * v;
+%l = 5 * vdT;
 
-l = 30; % corresponds to 5*dT*v
+l = 30; % corresponds to ish 5*dT*v
 dT_times_v = vdT;
 
 maxPos = max(max(M,[],1),[],3);
@@ -24,24 +16,80 @@ minPos = min(min(M,[],1),[],3);
 gridSize = ceil((maxPos - minPos) / l);
 areaGrid = zeros(gridSize);
 
+%lostSquares = 0;
 indexedPos_a = floor((M - minPos)/l) + 1;
-indexedNoFloor = (M - minPos)/l;
 
+extraX = zeros(size(indexedPos_a,3),1);
+extraY = zeros(size(indexedPos_a,3),1);
+fill = 1;
+
+for a = 1:size(indexedPos_a,1)
+    for i = 1:size(indexedPos_a,3)-1
+       if (norm(indexedPos_a(a,1,i)-indexedPos_a(a,1,i+1))>0 && norm(indexedPos_a(a,2,i)-indexedPos_a(a,2,i+1))>0)
+           %lostSquares = lostSquares + 1;
+           % blabla hittar rätt ruta
+           lineMovement = [M(a,1,i) M(a,2,i); M(a,1,i+1) M(a,2,i+1)];
+           ind = indexedPos_a(a,:,i);
+           lines = zeros(2,2,1);
+           lines(:,:,1) = [(ind(1)-1)*l+minPos(1) (ind(2)-1)*l+minPos(2); (ind(1)-1)*l+minPos(1) (ind(2))*l+minPos(2)];
+           lines(:,:,2) = [(ind(1)-1)*l+minPos(1) (ind(2))*l+minPos(2); ind(1)*l+minPos(1) ind(2)*l+minPos(2)];
+           lines(:,:,3) = [ind(1)*l+minPos(1) ind(2)*l+minPos(2); ind(1)*l+minPos(1) (ind(2)-1)*l+minPos(2)];
+           lines(:,:,4) = [ind(1)*l+minPos(1) (ind(2)-1)*l+minPos(2); (ind(1)-1)*l+minPos(1) (ind(2)-1)*l+minPos(2)];
+           
+           for j = 1:4
+               line = lines(:,:,j);
+               %Reorder dimentions
+               line = line(:,:);
+              if (~isnan(lineIntersection(lineMovement,line)))
+                  if (j == 1) 
+                      j
+                      xInd = ind(1)-1;
+                      yInd = ind(2);
+                  elseif j == 2
+                      j
+                      xInd = ind(1);
+                      yInd = ind(2)+1;   
+                  elseif j == 3
+                      j
+                      xInd = ind(1)+1;
+                      yInd = ind(2);
+                  elseif j == 4
+                      j
+                      xInd = ind(1)-1;
+                      yInd = ind(2);
+                  end
+                  
+                  extraX(fill) = xInd;
+                  extraY(fill) = yInd;
+                  fill = fill + 1;
+                  break;
+              end
+           end
+       end
+    end
+end
+lines(:,:,1);
 xIndices = indexedPos_a(:,1,:);
 yIndices = indexedPos_a(:,2,:);
 xIndices = xIndices(:,:)';
 yIndices = yIndices(:,:)';
 
+extraX ;
+extraY ;
 for i = 1:size(xIndices)
     xIndices(i);
     yIndices(i);
     areaGrid(xIndices(i),yIndices(i)) = 1;
+    if (extraX(i) ~= 0 || extraY(i) ~= 0)
+        5
+        areaGrid(extraX(i),extraY(i)) = 1;
+    end
 end
 
 numSquares = sum(sum(areaGrid)); 
 %A = l*l*squares;
-numTimeSteps = size(M,3)
+numTimeSteps = size(M,3);
 numOfAgents = size(M,1);
 % v*dT = 6 pixlar
-A = numSquares/(numOfAgents*dT_times_v*numTimeSteps/l)
+A = numSquares/(numOfAgents*dT_times_v*numTimeSteps/l);
 end
