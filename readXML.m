30ef161e68d92fdabdeaab721a1587b98bd5e39e
EbbaEkblom <ekebba@student.chalmers.se>
2019-03-07 11:43:52 +0100

    Can save and load data to/from file

--- readXML.m
+++ readXML.m
@@ -4,7 +4,7 @@ file = 'Filtered videos\2_Tracks.xml'; %Name of file
 agent = 1; % which agent/s we look at
 
 dT = 1/25; % Time step
-M = cut(file,agent); % Turns file into a position matrix, without NaN:s
+[M,length,times] = cut(file,agent); % Turns file into a position matrix, without NaN:s
 
 [kir,v] = getCirality(M,dT,1);
 
@@ -12,6 +12,28 @@ l = 30; % Corresponds to ~5*v*dT for agents in experiments
 [squares,normA] = calcArea(M,v,dT,l);
 
 result = [kir, normA]
+
+%% Run to save results to file
+clc;
+% Sparar:
+    % Filnamn/path (i en egen fil)
+    % Kiralitet
+    % NormArea
+    % Total tid
+    % hastighet
+    % l (size of area elements)
+
+expName = 'hm1agent'; %Change name for each new set of data
+
+file1 = ['results\Lab\' expName '.txt']; % Name of dataFile
+file2 = ['results\Lab\' expName 'SourceFiles.txt']; % Name of file containing names of XML files
+    
+data = [result size(M,3)*dT v l];
+dlmwrite(file1,data,'-append');
+
+fileID = fopen(file2,'a');
+fprintf(fileID,'%-40s\n',file);
+fclose(fileID);
 
 %% Plot M, movement of agent
 
@@ -28,21 +50,20 @@ obstacle = [0 0; 0 0];
 
 p = animateExperiment(M,obstacle,dT);
 
-%% Run to save workspace
-dateTime = clock;
-R_s = num2str(R);
-r_s = num2str(r);
-filename = strcat( join(string(dateTime(1:3)),''), '-', join(string(dateTime(4:5)),''), '_', obstacleType, R_s([1,3:end]), r_s([1,3:end]), '_', num2str(numAgents))
-path = strcat(pwd, '/results/', filename)
-save(path)
-%saveas(h,figname, 'fig')
 
 %% Plot graph for all chiralities
+
+experiment = 'hm1agent';
+dataFile = ['results\Lab\' experiment '.txt'];
+sourceFile = ['results\Lab\' experiment 'SourceFiles.txt'];
+allData = dlmread(dataFile);
+
+w = norm(allData(:,1));
+normArea = allData(:,2); 
+
 figure(112)
-%areaPerTime_max = numAgents*(4*v*totalTime*l/pi-l^2)/totalTime;
-areaPerTime_max = normA; %
-semilogx(w,areaPerTime/(areaPerTime_max),'o')
-axis([0.01, 10, 0, 1.2])
+semilogx(w,normArea,'o')
+%axis([0.01, 10, 0, 1.2])
 %name=strcat('step', num2str(dT), '; ', 'time', num2str(numTimeSteps), '; ', 'simulations', num2str(numSimulations), '; ', obstacleType, '; ', 'R=', num2str(R), '; D_r=', num2str(D_r));
 name = 'Experimental results';
 title(name)
