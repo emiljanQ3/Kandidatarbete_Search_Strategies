3275278cc5eab7c247751a093075d04310bc9c59
EbbaEkblom <ekebba@student.chalmers.se>
2019-03-07 14:35:01 +0100

    Added output arguments

--- cut.m
+++ cut.m
@@ -1,7 +1,8 @@
-function cutM = cut(file,agent)
+function [cutM,length, times] = cut(file,agent)
 % Reads an xml file into a matrix, for a specific agent
 % Removes all NaNs in the beginning and end of the matrix
 [M,lengths,times] = agentTracking(file);
-ind = 1;
 cutM = M(agent,:,times(agent,1):times(agent,2));
+length = lengths(agent);
+times = times(agent,:);
 end 
