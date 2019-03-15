92da80c664c413a1ee73202aad31997ed5715d90
sofiacd <sofiacd1996@gmail.com>
2019-03-15 13:12:44 +0100

    in progress, to loop through complex results

--- splitPositionData.m
+++ splitPositionData.m
@@ -1,21 +1,24 @@
-function pos_res = splitPositionData(pos_a)
+function [pos_res, indice] = splitPositionData(pos_a)
     figure(3004)
     
     X = pos_a(1, 1, :);
     Y = pos_a(1, 2, :);
     start = [pos_a(1,1,1), pos_a(1,2,1)];
     slut = [pos_a(1,1,size(pos_a,3)), pos_a(1,2,size(pos_a,3))];
+    
+    %indice= zeros(floor(size(pos_a, 3)/10), 2); %should be enough
 
     X = X(:,:)';
     Y = Y(:,:)';
     plot(X,Y);
     hold on
+    plot(X,Y, '.');
+
     plot(start(1),start(2), 'o', 'MarkerEdgeColor','r')
     plot(slut(1), slut(2),'o', 'MarkerEdgeColor','g')
     
     button = 0;
     agent = 0;
-    
     pos_res = zeros(1,2,length(pos_a(1,1,:)))
     while(button ~= 3)
         agent = agent +1;
@@ -45,11 +48,13 @@ function pos_res = splitPositionData(pos_a)
         
         if I(2)>I(1)
             pos_res(agent,:,1:(I(2)-I(1)+1)) = pos_a(1,:,I(1):I(2));
+            indice(agent, :) = [I(1); I(2)]; % start index; slut index
         else
             pos_res(agent,:,1:(I(1)-I(2)+1)) = pos_a(1,:,I(2):I(1));
+            indice(agent, :) = [I(2); I(1)]; % start index; slut index
         end
 
     end
-    
+    indice(agent,:) = NaN
     p = 5;
 end
