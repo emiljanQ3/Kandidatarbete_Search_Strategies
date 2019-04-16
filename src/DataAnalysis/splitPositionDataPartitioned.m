2a2321a1e3a9778583ad1c1406bb0c80d16a9233
Emil <emiljanQ3@hotmail.com>
2019-04-16 11:48:32 +0200

    Fixed bug

--- src/DataAnalysis/splitPositionDataPartitioned.m
+++ src/DataAnalysis/splitPositionDataPartitioned.m
@@ -1,31 +1,28 @@
 function [pos_res, indice] = splitPositionDataPartitioned(pos_a, n, myCircle)
-   pos_res = zeros([1,2,n]);
-   indice  = zeros([1,2])
-   
-   k = 1;
-   while k + n - 1 <= length(pos_a)
-       [dPos, dIndice] = splitPositionData(pos_a(:,:,k: k + n - 1), myCircle);
+    pos_res = zeros([1,2,n]);
+    indice  = zeros([1,2]);
+
+    k = 0;
+    while k + n <= length(pos_a)
+       [dPos, dIndice] = splitPositionData(pos_a(:,:,k + 1: k + n), myCircle);
        if(size(dIndice,1)>1)
             pos_res = [pos_res ; dPos];
-            indice  = [indice ; dIndice(1:end-1,:)];
-       else
-          0
-       end 
+       end
+       indice  = [indice ; k + dIndice(1:end-1,:)];
        k = k + n;
-   end
+    end
    
-   %Last interation
-      [dPos, dIndice] = splitPositionData(pos_a(:,:,k:end), myCircle);
+    %Last interation
+    [dPos, dIndice] = splitPositionData(pos_a(:,:,k + 1:end), myCircle);
     if(size(dIndice,1)>1)
-      dPos = cat(3, dPos , zeros(size(dPos,1), size(dPos,2), n - size(dPos,3)));
-      pos_res = [pos_res ; dPos];
-      indice  = [indice ; dIndice];
-   else 
-       3
-   end 
-   %Remove first row
-   pos_res = pos_res(2:end,:,:);
-   indice  = indice(2:end,:);
-   
-   close all
+        dPos = cat(3, dPos , zeros(size(dPos,1), size(dPos,2), n - size(dPos,3)));
+        pos_res = [pos_res ; dPos];
+    end 
+    indice  = [indice ; k + dIndice];
+    
+    %Remove first row
+    pos_res = pos_res(2:end,:,:);
+    indice  = indice(2:end,:);
+
+    close all
 end
