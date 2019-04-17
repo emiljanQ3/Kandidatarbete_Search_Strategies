45e408bd338dfed378aa0e0def2e79ebe748862d
Kevin Andersson <kevin@Kevins-MacBook-Pro.local>
2019-04-17 11:24:48 +0200

    still temporely fix not used

--- src/Util/Intersects.m
+++ src/Util/Intersects.m
@@ -4,6 +4,7 @@ function bool = Intersects(p1, q1, p2, q2)
     o3 = orientation(p2, q2, p1); 
     o4 = orientation(p2, q2, q1);
     
+    o4;
     if (o1 ~= o2 && o3 ~= o4) 
         bool = true;
         return;
