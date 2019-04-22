c869a1fd52ebd69f280473ea62a6091644ac8e6a
kevan97 <kevinan@student.chalmers.se>
2019-04-22 10:34:40 +0200

    Can now plot legends with the rigth chirality and with latex symbols

--- src/Util/plottingData.m
+++ src/Util/plottingData.m
@@ -1,7 +1,7 @@
 %% Simulated results
 
 load('results/Final_results/2019417-1057_circle_R1_t180_l0156.mat')
-    T       = 50;         % vid vilken tidpunkt plottar vi resultatet mot kiralitet
+    T       = 180;         % vid vilken tidpunkt plottar vi resultatet mot kiralitet
     T2      = 180;         % Hur lÃ¥ng tid plottar vi arean 
     index2  = floor(numAreaDP*T2/measurmentTime);           
     index   = floor(numAreaDP*T/measurmentTime);
@@ -28,9 +28,10 @@ load('results/Final_results/2019417-1057_circle_R1_t180_l0156.mat')
     axis([0.1 10 0 1])
     title('')
 %% Experimental results
+clc
 load('results/Final_results/2019417-1959_circle_medium_1agent_t50_l216.mat')
 clear 'length'
-N_k = 15;
+N_k = 8;
 
 figure(110)
 hold on
@@ -56,8 +57,11 @@ hold on
 
 c = chir2color(binKir)
 map = colormap(c);
+format short
 for i = 1:size(meanArea,2)
-    P(i) = plot((1:size(meanArea,1))/2, meanArea(:,i)/maxArea,'color',c(i,:))
+    num = round(binKir(i),3,'significant');
+    txt = ['$\omega$ = ',num2str(num),' rad/s'];
+    P(i) = plot((1:size(meanArea,1))/2, meanArea(:,i)/maxArea,'color',c(i,:),'DisplayName',txt,'Linewidth', 4)
 end
 
 bar = colorbar
@@ -72,7 +76,7 @@ figure(112)
 hold on
 c = chir2color(binKir)
 P2 = semilogx(binKir,meanArea(index,:)/maxArea,'k.','markersize',30)
-
+set(gca,'xscale','log')
 % for i = 1:length(binKir)
 %     semilogx(binKir(i), meanArea(index,i)/maxArea,'.','color',c(i,:),'markersize',30)
 % end
@@ -94,12 +98,14 @@ legend([P1 P2], 'Simulering','Experimentell')
 figure(109)
 
 axis([0 50 0 1])
-title('Experimentell data av normerad uppsÃ¶kt area Ã¶ver tid', 'Interpreter', 'latex')      %titla
+title('Experimentell data av normerad uppsökt area över tid', 'Interpreter', 'latex')      %titla
 set(gca, 'fontsize', 30)
 
-ylabel('Area ', 'Interpreter', 'latex', 'fontsize', 50)
-xlabel('Kiralitet (rad/s)', 'Interpreter', 'latex', 'fontsize', 50)
-legend(P,binKir)
+ylabel('Area () ', 'Interpreter', 'latex', 'fontsize', 50)
+xlabel('Tid (s)', 'Interpreter', 'latex', 'fontsize', 50)
+
+legend('show','Location','northwest','Interpreter','latex')
+
 
 %% set font sizes and titel on figur 111
 
