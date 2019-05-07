e22dd96de0e032787cff1dfb282fd2ef9b671378
Emil <emiljanQ3@hotmail.com>
2019-05-07 16:23:59 +0200

    Added new colorscale with yellow

---
+++ src/Util/chir2color2.m
@@ -0,0 +1,12 @@
+function c = chir2color(w)
+    palette = getYGBGradient(0.4,0.5,0.6,1001);
+    c = zeros(length(w),3);
+    for i = 1:length(w)
+        if (w(i) < 0.1)
+            c(i,:) = palette(1,:);
+        else
+            c(i,:) = palette(ceil((log10(w(i))+1)*500)+1,:);
+        end
+    end
+    
+end
